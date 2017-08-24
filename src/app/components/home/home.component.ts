4ad31296cf4543e64560e065097c8b00849bd758
krustydaclown <s.fourault@gmail.com>
2017-08-24 16:25:44 +0200

    Reco Learning Path V0

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -10,10 +10,26 @@ import { overlayConfigFactory } from "ngx-modialog";
 import { BSModalContext } from 'ngx-modialog/plugins/bootstrap';
 import { ToolmodalComponent } from '../toolmodal/toolmodal.component';
 
+import { trigger, state, style, animate, transition } from '@angular/animations';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  animations: [
+      trigger('slideUp', [
+        state('hidden', style({
+          height: '0',
+          opacity: 0
+        })),
+        state('visible',   style({
+          height: '*',
+          opacity: 1
+        })),
+        transition('hidden => visible', animate('300ms ease-in')),
+        transition('visible => hidden', animate('500ms ease-in-out'))
+      ])
+    ]
 })
 export class HomeComponent implements OnInit {
 
@@ -22,7 +38,10 @@ export class HomeComponent implements OnInit {
   public learningPath;
   public idLearningPath;
   public paths;
-  public selectedValue;
+  public selectedLearningPath;
+  public headerState = "visible";
+  public recoState = "hidden";
+  public recoTools;
 
   constructor( private dataService : DataService, public modal: Modal, private route: ActivatedRoute  ) { }
 
@@ -44,11 +63,18 @@ export class HomeComponent implements OnInit {
     this.dataService.getTable("LearningPaths").then( data => {
       this.paths = data; 
       var randomGoal = Math.floor((Math.random() * data.records.length));
-      this.selectedValue = this.paths.records[randomGoal];
+      this.selectedLearningPath = this.paths.records[randomGoal];
     });
 
     this.dataService.getTable("Tools").then( data => this.tools = data );
     this.dataService.getTable("Experts").then( data => this.experts = data );
+  }
+
+  showLearningPath() {
+    this.headerState = "hidden";
+    this.recoState = "visible";
+    this.recoTools = this.tools.records.filter(x => this.selectedLearningPath.fields['Tools'].includes(x.id) );
+    console.log(this.recoTools);
   }
 
   openModal(tool) {
